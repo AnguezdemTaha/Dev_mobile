b3c792f6678cfb0dd95241baf84a176c2d3c4f83
FZ <nakachfz@gmail.com>
2020-07-07 18:49:38 +0100

    Revert "probeleme version"
    This reverts commit 4e865a4ebdd9c6d65bb529e5be60eaeadeb30587.

---
+++ src/ConsultationBundle/Entity/Docteur.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace ConsultationBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Docteur
+ *
+ * @ORM\Table(name="docteur")
+ * @ORM\Entity(repositoryClass="ConsultationBundle\Repository\DocteurRepository")
+ */
+class Docteur
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+     /**
+     * @var string
+     *
+     * @ORM\Column(name="specialite", type="string", length=255)
+     */
+    private $specialite;
+
+ 
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Docteur
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Docteur
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function __toString() {
+        return $this->nom;
+    }
+
+    /**
+     * Set specialite
+     *
+     * @param string $specialite
+     *
+     * @return Docteur
+     */
+    public function setSpecialite($specialite)
+    {
+        $this->specialite = $specialite;
+
+        return $this;
+    }
+
+    /**
+     * Get specialite
+     *
+     * @return string
+     */
+    public function getSpecialite()
+    {
+        return $this->specialite;
+    }
+
+    
+
+}
