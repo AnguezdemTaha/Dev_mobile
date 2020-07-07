4e865a4ebdd9c6d65bb529e5be60eaeadeb30587
FZ <nakachfz@gmail.com>
2020-07-07 18:19:12 +0100

    probeleme version

--- app/src/main/java/com/example/myapplication12/Model/Participation.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication12.Model;
-
-public class Participation {
-    //.
-    private  Personne Per_participe;
-    private  Evenement Evn_participe;
-}
--- src/ConsultationBundle/ConsultationBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ConsultationBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ConsultationBundle extends Bundle
-{
-}
